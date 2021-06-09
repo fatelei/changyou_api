4387edc207399856ca62d96645b421e97b7c11b9
fatelei <fatelei@gmail.com>
2021-06-09 15:23:49 +0800

    chore: generate session_id

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="changyou",
-    version="1.0.0",
+    version="1.0.1",
     author="fatelei",
     author_email="fatelei@gmail.com",
     description="changyou api client",
--- src/changyou/tongdun.py
+++ src/changyou/tongdun.py
@@ -1,5 +1,7 @@
 import asyncio
+import time
 import os
+import random
 from pyppeteer import launch
 
 
@@ -10,16 +12,16 @@ blackbox_html = os.path.join(dirname, 'blackbox.html')
 
 class Tongdun(object):
 
-    async def async_get_session_id(self):
-        browser = await launch(headless=True,
-                               handleSIGINT=False,
-                               handleSIGTERM=False,
-                               handleSIGHUP=False)
-        page = await browser.newPage()
-        await page.goto(f'file://{tfd_html}', {'waitUntil': 'networkidle2'})
-        session_id = await page.evaluate('''() => sessionId''')
-        await browser.close()
-        return session_id
+    # async def async_get_session_id(self):
+    #     browser = await launch(headless=True,
+    #                            handleSIGINT=False,
+    #                            handleSIGTERM=False,
+    #                            handleSIGHUP=False)
+    #     page = await browser.newPage()
+    #     await page.goto(f'file://{tfd_html}', {'waitUntil': 'networkidle2'})
+    #     session_id = await page.evaluate('''() => sessionId''')
+    #     await browser.close()
+    #     return session_id
 
     async def async_get_blackbox(self):
         browser = await launch(headless=True,
@@ -33,12 +35,9 @@ class Tongdun(object):
         return blackbox
 
     def get_session_id(self):
-        try:
-            loop = asyncio.get_event_loop()
-        except RuntimeError:
-            loop = asyncio.new_event_loop()
-        session_id = loop.run_until_complete(self.async_get_session_id())
-        return session_id
+        ts = int(time.time() * 1000)
+        suffix = random.uniform(0, 1).hex()[4:-3]
+        return f'changyo-pc-{ts}-{suffix}'
 
     def get_blackbox(self):
         try:
